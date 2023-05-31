6d027fd1a1863d175d07d1880a75d21026c8a6be
masaru suzuki <masaru.suzuki.yamagata@gmail.com>
2023-05-31 15:34:52 +0900

    add: ドメインロジックのざっくりとした流れを実装

--- src/domain/board.ts
+++ src/domain/board.ts
@@ -1,5 +1,23 @@
 import { Disc } from './disk';
+import { Move } from './move';
 
 export class Board {
   constructor(private _discs: Disc[][]) {}
+
+  place(move: Move): Board {
+    // 盤面に石を置けるかチェックする
+
+    // ボードをコピーする(バグに繋がらないようにするため)
+    const newBoard = this._discs.map((line) => {
+      return line.map((disc) => disc);
+    });
+
+    // 石を置く
+    newBoard[move.point.y][move.point.x] = move.disc;
+
+    // ひっくり返す
+
+    // ボードを返す
+    return new Board(newBoard);
+  }
 }
--- src/domain/turn.ts
+++ src/domain/turn.ts
@@ -1,6 +1,7 @@
 import { Board } from './board';
 import { Disc } from './disk';
 import { Move } from './move';
+import { Point } from './point';
 
 export class Turn {
   constructor(
@@ -11,4 +12,20 @@ export class Turn {
     private _board: Board,
     private _endAt: Date
   ) {}
+
+  placeNext(disc: Disc, point: Point): Turn {
+    // TODO: 次の石ではない場合は、エラーを返す
+    if (disc !== this._nextDisc) {
+      throw new Error('次の石ではありません');
+    }
+
+    const move = new Move(disc, point);
+
+    const newBoard = this._board.place(move);
+
+    // TODO: 石を置けない場合はスキップする
+    const nextDisc = this._nextDisc === Disc.DARK ? Disc.LIGHT : Disc.DARK;
+
+    return new Turn(this._gameId, this._turnCount + 1, nextDisc, move, newBoard, new Date());
+  }
 }
