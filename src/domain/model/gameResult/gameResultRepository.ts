8a3b2eff37b0beee8e8ece8582db04562f59eff9
masaru suzuki <masaru.suzuki.yamagata@gmail.com>
2023-06-03 16:24:20 +0900

    mod: 設計をクリーンアーキテクチャーにするために、infrastructure層にrepositoryディレクトリを作成。ドメイン層のgameResultRepositoryをインターフェースとして使い、実装はinfrastructure層のgameResultMySQLRepository.tsで行う

--- src/domain/model/gameResult/gameResultRepository.ts
+++ src/domain/model/gameResult/gameResultRepository.ts
@@ -3,17 +3,7 @@ import { GameResult } from './gameResult';
 import { GameResultGateway } from '../../../infrastructure/gameResultGateway';
 import { toWinnerDisc } from './winnerDisc';
 
-const gameResultGateway = new GameResultGateway();
-export class GameResultRepository {
-  async findForGameId(conn: mysql.Connection, gameId: number): Promise<GameResult | undefined> {
-    const gameResultRecord = await gameResultGateway.findForGameId(conn, gameId);
-
-    if (!gameResultRecord) return undefined;
-
-    return new GameResult(gameResultRecord.gameId, toWinnerDisc(gameResultRecord.winnerDisc), gameResultRecord.endAt);
-  }
-
-  async save(conn: mysql.Connection, gameResult: GameResult) {
-    await gameResultGateway.insert(conn, gameResult);
-  }
+export interface GameResultRepository {
+  findForGameId(conn: mysql.Connection, gameId: number): Promise<GameResult | undefined>;
+  save(conn: mysql.Connection, gameResult: GameResult): Promise<void>;
 }
