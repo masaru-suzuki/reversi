3fc1d7e4699980703ae42d8f7be608bd89808414
masaru suzuki <masaru.suzuki.yamagata@gmail.com>
2023-06-03 15:08:54 +0900

    mod: 設計をクリーンアーキテクチャーにするために、infrastructure層にrepositoryディレクトリを作成。ドメイン層のgameRepositoryをインターフェースとして使い、実装はinfrastructure層のgameMySQLRepository.tsで行う

--- src/domain/model/game/gameRepository.ts
+++ src/domain/model/game/gameRepository.ts
@@ -1,23 +1,8 @@
 import mysql from 'mysql2/promise';
-import { GameGateway } from '../../../infrastructure/gameGateway';
+import { GameGateway } from '../../../infrastructure/repository/game/gameGateway';
 import { Game } from './game';
 
-const gameGateway = new GameGateway();
-
-export class GameRepository {
-  // 戻り値も設定する
-  async findLatest(conn: mysql.Connection): Promise<Game | undefined> {
-    const gameRecord = await gameGateway.findLatest(conn);
-
-    if (!gameRecord) {
-      return undefined;
-    } else {
-      return new Game(gameRecord.id, gameRecord.startedAt);
-    }
-  }
-
-  async save(conn: mysql.Connection, game: Game): Promise<Game> {
-    const gameRecord = await gameGateway.insert(conn, game.startedAt);
-    return new Game(gameRecord.id, gameRecord.startedAt);
-  }
+export interface GameRepository {
+  findLatest(conn: mysql.Connection): Promise<Game | undefined>;
+  save(conn: mysql.Connection, game: Game): Promise<Game>;
 }
---
+++ src/infrastructure/repository/game/gameMySQLRepository.ts
@@ -0,0 +1,26 @@
+import mysql from 'mysql2/promise';
+import { GameResult } from '../../../domain/model/gameResult/gameResult';
+import { GameResultGateway } from '../../gameResultGateway';
+import { toWinnerDisc } from '../../../domain/model/gameResult/winnerDisc';
+import { GameRepository } from '../../../domain/model/game/gameRepository';
+import { Game } from '../../../domain/model/game/game';
+import { GameGateway } from './gameGateway';
+
+const gameGateway = new GameGateway();
+
+export class gameMySQLRepository implements GameRepository {
+  async findLatest(conn: mysql.Connection): Promise<Game | undefined> {
+    const gameRecord = await gameGateway.findLatest(conn);
+
+    if (!gameRecord) {
+      return undefined;
+    } else {
+      return new Game(gameRecord.id, gameRecord.startedAt);
+    }
+  }
+
+  async save(conn: mysql.Connection, game: Game): Promise<Game> {
+    const gameRecord = await gameGateway.insert(conn, game.startedAt);
+    return new Game(gameRecord.id, gameRecord.startedAt);
+  }
+}
